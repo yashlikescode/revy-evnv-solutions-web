e0007f7d65a38896047556fe56ee527d9a237fcc
Yashasvi <kmryashasvi@gmail.com>
2024-02-10 23:15:20 +0530

    added landing page

--- app.js
+++ app.js
@@ -2,57 +2,55 @@ function scrollToElement(elementId) {
   var targetElement = document.getElementById(elementId);
 
   if (targetElement) {
-    var offsetTop = targetElement.offsetTop -115;
+    var offsetTop = targetElement.offsetTop - 115;
     window.scrollTo({
       top: offsetTop,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
   }
 }
 
-  // Get a reference to the iframe
-  var iframe = document.getElementById('contform');
+// Get a reference to the iframe
+var iframe = document.getElementById("contform");
 
-  // Check if the iframe has loaded
-  iframe.addEventListener('load', function () {
-    // Access the iframe's content document
-    var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+// Check if the iframe has loaded
+iframe.addEventListener("load", function () {
+  // Access the iframe's content document
+  var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
 
-    // Get the form element within the iframe
-    var form = iframeDoc.getElementById('contact-form'); // Replace with your form's ID
-    var thank = document.getElementById('thank');
-    // Add a submit event listener to the form
-    form.addEventListener('submit', function (event) {
-      iframe.style.display='none';
-      thank.removeAttribute("hidden");
-    });
+  // Get the form element within the iframe
+  var form = iframeDoc.getElementById("contact-form"); // Replace with your form's ID
+  var thank = document.getElementById("thank");
+  // Add a submit event listener to the form
+  form.addEventListener("submit", function (event) {
+    iframe.style.display = "none";
+    thank.removeAttribute("hidden");
   });
+});
 
-
-  let items = document.querySelectorAll('.carousel .carousel-item')
+let items = document.querySelectorAll(".carousel .carousel-item");
 
 items.forEach((el) => {
-    const minPerSlide = 4
-    let next = el.nextElementSibling
-    for (var i=1; i<minPerSlide; i++) {
-        if (!next) {
-            // wrap carousel by using first child
-        	next = items[0]
-      	}
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
+  const minPerSlide = 4;
+  let next = el.nextElementSibling;
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = items[0];
     }
-})
-
-
-
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
 
 // Get the button
 let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -63,45 +61,49 @@ function scrollFunction() {
 }
 
 function vanitalinkedin() {
-  var linkToOpen = 'https://www.linkedin.com/in/vanita-prasad-92310551/';
-  window.open(linkToOpen, '_blank');
+  var linkToOpen = "https://www.linkedin.com/in/vanita-prasad-92310551/";
+  window.open(linkToOpen, "_blank");
 }
 function rajneeshlinkedin() {
-  var linkToOpen = 'https://www.linkedin.com/in/rajneesh-prasad-29419312/';
-  window.open(linkToOpen, '_blank');
+  var linkToOpen = "https://www.linkedin.com/in/rajneesh-prasad-29419312/";
+  window.open(linkToOpen, "_blank");
 }
 function bhaviklinkedin() {
-  var linkToOpen = 'https://www.linkedin.com/in/bhavikkumar-kathrani-11255013b/';
-  window.open(linkToOpen, '_blank');
+  var linkToOpen =
+    "https://www.linkedin.com/in/bhavikkumar-kathrani-11255013b/";
+  window.open(linkToOpen, "_blank");
 }
 
 function xeon() {
-  var linkToOpen = './assets/testimonials/4.png';
-  window.open(linkToOpen, '_blank');
+  var linkToOpen = "./assets/testimonials/4.png";
+  window.open(linkToOpen, "_blank");
 }
 function stagot() {
-  var linkToOpen = './assets/testimonials/3.png';
-  window.open(linkToOpen, '_blank');
+  var linkToOpen = "./assets/testimonials/3.png";
+  window.open(linkToOpen, "_blank");
 }
 function transpeck() {
-  var linkToOpen = './assets/testimonials/1.png';
-  window.open(linkToOpen, '_blank');
+  var linkToOpen = "./assets/testimonials/1.png";
+  window.open(linkToOpen, "_blank");
 }
 function pepsico() {
-  var linkToOpen = './assets/testimonials/2.jpg';
-  window.open(linkToOpen, '_blank');
+  var linkToOpen = "./assets/testimonials/2.jpg";
+  window.open(linkToOpen, "_blank");
 }
 
 function addPaddingForMobile() {
-  var targetElement = document.getElementById('revynav');
-  var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  var targetElement = document.getElementById("revynav");
+  var screenWidth =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
 
   if (screenWidth < 600) {
     // Add padding for smartphones
-    targetElement.style.paddingTop = '2%';
+    targetElement.style.paddingTop = "2%";
   } else {
     // Reset padding for larger screens
-    targetElement.style.paddingTop = '1%';
+    targetElement.style.paddingTop = "1%";
   }
 }
 
@@ -109,11 +111,10 @@ function addPaddingForMobile() {
 addPaddingForMobile();
 
 // Listen for window resize events to update padding dynamically
-window.addEventListener('resize', addPaddingForMobile);
+window.addEventListener("resize", addPaddingForMobile);
 
-
-window.addEventListener('scroll', function() {
-  var textElement6 = document.querySelector('#home-article');
+window.addEventListener("scroll", function () {
+  var textElement6 = document.querySelector("#home-article");
   var rect6 = textElement6.getBoundingClientRect();
   var navInd6 = document.getElementById("nav-home");
   if (rect6.top <= window.innerHeight && rect6.bottom >= window.innerHeight) {
@@ -122,11 +123,10 @@ window.addEventListener('scroll', function() {
   } else {
     navInd6.style.color = "black";
     navInd6.style.textDecoration = "none";
-
   }
 });
-window.addEventListener('scroll', function() {
-  var textElement6 = document.querySelector('#about-article');
+window.addEventListener("scroll", function () {
+  var textElement6 = document.querySelector("#about-article");
   var rect6 = textElement6.getBoundingClientRect();
   var navInd6 = document.getElementById("nav-about");
   if (rect6.top <= window.innerHeight && rect6.bottom >= window.innerHeight) {
@@ -137,8 +137,8 @@ window.addEventListener('scroll', function() {
     navInd6.style.textDecoration = "none";
   }
 });
-window.addEventListener('scroll', function() {
-  var textElement6 = document.querySelector('#service-article');
+window.addEventListener("scroll", function () {
+  var textElement6 = document.querySelector("#service-article");
   var rect6 = textElement6.getBoundingClientRect();
   var navInd6 = document.getElementById("nav-services");
   if (rect6.top <= window.innerHeight && rect6.bottom >= window.innerHeight) {
@@ -149,8 +149,8 @@ window.addEventListener('scroll', function() {
     navInd6.style.textDecoration = "none";
   }
 });
-window.addEventListener('scroll', function() {
-  var textElement6 = document.querySelector('#gallery-article');
+window.addEventListener("scroll", function () {
+  var textElement6 = document.querySelector("#gallery-article");
   var rect6 = textElement6.getBoundingClientRect();
   var navInd6 = document.getElementById("nav-gallery");
   if (rect6.top <= window.innerHeight && rect6.bottom >= window.innerHeight) {
@@ -161,8 +161,8 @@ window.addEventListener('scroll', function() {
     navInd6.style.textDecoration = "none";
   }
 });
-window.addEventListener('scroll', function() {
-  var textElement6 = document.querySelector('#contact-us-article');
+window.addEventListener("scroll", function () {
+  var textElement6 = document.querySelector("#contact-us-article");
   var rect6 = textElement6.getBoundingClientRect();
   var navInd6 = document.getElementById("nav-contactus");
   if (rect6.top <= window.innerHeight) {
@@ -173,3 +173,30 @@ window.addEventListener('scroll', function() {
     navInd6.style.textDecoration = "none";
   }
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const changingText = document.getElementById("changingText");
+  const texts = [
+    "Are You Concerned About Your Biogas Plant's Efficiency?",
+    "Are You Facing Challenges in STP Management?",
+    "Having Issues in  ETP Operations?",
+  ];
+
+  let index = 0;
+
+  function changeText() {
+    changingText.textContent = texts[index];
+    index = (index + 1) % texts.length;
+  }
+
+  function fadeInOut() {
+    changingText.style.opacity = 1;
+    setTimeout(function () {
+      changingText.style.opacity = 0;
+      setTimeout(changeText, 1000); // Change text after fade out
+      setTimeout(fadeInOut, 1000); // Call fadeInOut again after changing text
+    }, 3000); // Wait for 3 seconds before starting fade out
+  }
+
+  fadeInOut(); // Start the fading in and out process
+});
+
