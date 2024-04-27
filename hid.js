e4ea3c8e44566efcfc5e961a6f2ad4709b56b7ff
Yashasvi <kmryashasvi@gmail.com>
2024-04-27 15:07:36 +0530

    added products and vertical animation

--- hid.js
+++ hid.js
@@ -8,3 +8,14 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+const observer2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("showv");
+    }
+  });
+});
+
+const hiddenElements2 = document.querySelectorAll(".hiddenv");
+hiddenElements2.forEach((el) => observer2.observe(el));
